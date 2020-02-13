5669e15f504081cd92e7c50ad3cdf59140e7eb4a
Ross <rosso5792@gmail.com>
2020-02-13 13:53:19 +0000

    checked deck starts empty - works

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -4,5 +4,11 @@ public class Deck {
 
     private ArrayList<Card> cards;
 
+    public Deck(){
+        cards = new ArrayList<Card>();
+    }
 
+    public int cardCount() {
+        return this.cards.size();
+    }
 }
